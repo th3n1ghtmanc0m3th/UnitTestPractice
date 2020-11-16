0b4b8f3ea62dc9fc8f352187278ccdc5b1183c40
th3n1ghtmanc0m3th <jaywehrman@gmail.com>
2020-11-16 13:24:24 -0800

    tested both programs

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -17,6 +17,31 @@ class PracticeTest : public ::testing::Test
 TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
-    bool actual = obj.isPalindrome("Not a palindrome");
+    bool actual = obj.isPalindrome("racecar");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_not_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_first_smallest)
+{
+    Practice obj;
+    int first = 5;
+    int second = 7;
+    int third = 2;
+    int* firstvalue = &first;
+    int* secondvalue = &second;
+    int* thirdvalue = &third;
+    //*first = &firstvalue;
+    //*second = &secondvalue; 
+    //*third = &thirdvalue;
+    obj.sortDescending(*firstvalue, *secondvalue, *thirdvalue);
+    ASSERT_EQ(first, 7);
+    ASSERT_EQ(second, 5);
+    ASSERT_EQ(third, 2);
 }
